e69e2b1d26b063942091256827bcbdc581944050
blitterated <blitterated@protonmail.com>
2024-03-30 02:13:00 -0500

    More demos, inverting color, and some cleanup
    Added demo methods for all permutations of r, g, and b.
    Added separate_colors() to show how each color spans a row set.
    First shot at iteration order of foreground and background.
    Above done with "invert" parameter.
    Better naming of vars in TileSeparatorGen.
    Generalized rgb_list() into list_color_pairs().

--- term_colors.rb
+++ term_colors.rb
@@ -57,23 +57,23 @@ module TerminalColors
 
   class TileSeparatorGen
     def initialize
-      @tile_count = 0
+      @tile_layer_count = 0
 
-      @next_full_tile_row_newline = "\n\n"
-      @next_tile_layer_newline    = "\n"
-      @tile_spacer                = "  "
+      @row_separator         = "\n\n"
+      @row_layer_separator   = "\n"
+      @tile_layer_separator  = "  "
     end
 
     def next_sep
-      @tile_count += 1
+      @tile_layer_count += 1
 
       separator = case
-      when @tile_count % 36 == 0; @next_full_tile_row_newline
-      when @tile_count % 6 == 0;  @next_tile_layer_newline
-      else ;                      @tile_spacer
+      when @tile_layer_count % 36 == 0; @row_separator
+      when @tile_layer_count % 6 == 0;  @row_layer_separator
+      else ;                            @tile_layer_separator
       end
 
-      #puts %Q(sep: #{separator.inspect}, count:#{@tile_count})
+      #puts %Q(sep: #{separator.inspect}, count:#{@tile_layer_count})
 
       separator
     end
@@ -104,70 +104,95 @@ module TerminalColors
     print ansi_output
   end
 
+  # Was using Array#repeated_permutation() until I read it doesn't guarantee ordering.
+  def build_color_pairs(color_cube, invert)
+
+    # Create an array that repeats color_cube's elements one at a time, Array#size times each.
+    slow_changer = color_cube.then do |cc|
+      cc.inject([]) do |acc, a|
+        acc.push(*([a] * cc.size))
+      end
+    end
+
+    # Multiply color_cube by Array#size to repeat all elements in order, Array#size times.
+    fast_changer = color_cube * color_cube.size
+
+    return fast_changer.zip(slow_changer) if (invert)
+    return slow_changer.zip(fast_changer)
+  end
+
+  def show_tiles(color_cube, invert = false)
+    gen = TileSeparatorGen.new
+
+    build_color_pairs(color_cube, invert).each do |fg, bg|
+      show_colors_and_text(fg, bg, gen.next_sep)
+    end
+    nil
+  end
+
+  def show_tiles_only(color_cube, invert = false)
+    gen = TileSeparatorGen.new
+
+    color_cube.each do |cp|
+      show_colors_and_text(cp, cp, gen.next_sep)
+    end
+    nil
+  end
+
+  def list_color_pairs(color_cube, invert)
+    build_color_pairs(color_cube, invert).each do |c1, c2|
+       puts  "#{c1}   |   #{c2}"
+    end
+    nil
+  end
+
+  # FEED ME A SYMBOL
+  def separate_colors(cube_sort)
+    row_set = AnsiColorCube.send(cube_sort)[0..215]
+
+    # 16 + (36 * r) + (6 * g) + b
+    blue_cube  = row_set.map { |cp| ColorPoint.new(0, 0, 0, cp.b + 16) }
+    green_cube = row_set.map { |cp| ColorPoint.new(0, 0, 0, (6 * cp.g) + 16) }
+    red_cube   = row_set.map { |cp| ColorPoint.new(0, 0, 0, (36 * cp.r) + 16) }
+
+    cube_sort.to_s.each_char do |c|
+      case c
+      when 'r'; show_tiles_only(red_cube)
+      when 'g'; show_tiles_only(green_cube)
+      when 'b'; show_tiles_only(blue_cube)
+      else    ; throw %Q("#{c}" is not a sortable color)
+      end
+    end
+  end
+
   class Demos
 
     extend TerminalColors
+    private def initialize; end;
 
     class << self
 
-      # Was using Array#repeated_permutation() until I read it doesn't guarantee ordering.
-      def build_color_pairs(color_cube)
-
-        # Create an array that repeats color_cube's elements one at a time, Array#size times each.
-        slow_changer = color_cube.then do |cc|
-          cc.inject([]) do |acc, a|
-            acc.push(*([a] * cc.size))
-          end
-        end
-
-        # Multiply color_cube by Array#size to repeat all elements in order, Array#size times.
-        fast_changer = color_cube * color_cube.size
-
-        slow_changer.zip(fast_changer)
-      end
-
-      def show_tiles(color_cube)
-        gen = TileSeparatorGen.new
-
-        build_color_pairs(color_cube).each do |fg, bg|
-          show_colors_and_text(fg, bg, gen.next_sep)
-        end
-        nil
-      end
-
+      # Each one of the [rgb]{3} methods below uses colors sorted in a particular order.
+      # The ordering is specified by the method names, e.g. bgr() means the colors are sorted
+      # by Blue first, Green second, and Red last.
+      #
+      # The color sorted first will progessively increase with each tile across a given row of tiles.
+      # The color sorted second will progessively increase down the row layers in a given row of tiles.
+      # The color sorted last will increase going down six rows of tiles.
+      # The value for the color sorted last will be the same for all layers in all tiles in a given row.
+      #
       # Each layer across a line of tiles shows each progessive background value of Blue.
       # Each layer across a line of tiles shows a progressive background value of Green.
       # Each line of tiles shows a progressive background value of Red for 6 lines / 36 tiles.
       # Each set of 36 tiles shows a single progression of foreground color through B, G, and R.
-      def bgr; show_tiles(AnsiColorCube.bgr); end
+      def bgr(invert = false); show_tiles(AnsiColorCube.bgr, invert); end
+      def brg(invert = false); show_tiles(AnsiColorCube.brg, invert); end
+      def gbr(invert = false); show_tiles(AnsiColorCube.gbr, invert); end
+      def grb(invert = false); show_tiles(AnsiColorCube.gbr, invert); end
+      def rbg(invert = false); show_tiles(AnsiColorCube.rbg, invert); end
+      def rgb(invert = false); show_tiles(AnsiColorCube.rgb, invert); end
 
-      # Each layer across a line of tiles shows each progessive foreground value of Blue.
-      # Each layer across a line of tiles shows a progressive foreground value of Green.
-      # Each line of tiles shows a progressive foreground value of Red for 6 lines / 36 tiles.
-      # Each set of 36 tiles shows a single progression of background color through B, G, and R.
-      def bgr_inv
-        Class.new do
-          include TerminalColors
-          def show_tiles()
-            gen = TileSeparatorGen.new
-            AnsiColorCube.bgr.repeated_permutation(2).each do |bg, fg| # <== Reversed Args!
-              show_colors_and_text(fg, bg, gen.next_sep)
-            end
-          end
-        end.new.show_tiles
-        nil
-      end
-
-      def rgb_list
-        build_color_pairs(AnsiColorCube.rgb).each do |c1, c2|
-           puts  "#{c1}   |   #{c2}"
-        end
-        nil
-      end
-
-      def rgb; show_tiles(AnsiColorCube.rgb); end
-
-      def gbr; show_tiles(AnsiColorCube.gbr); end
+      def rgb_list(invert = false); list_color_pairs(AnsiColorCube.rgb, invert); end
 
     end
   end
